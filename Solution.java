028b2e8e762cd9584c2b0c7ac308cfdcb8752c4e
Sanjay Kumar Yadav <sanjay.script.uk@gmail.com>
2025-03-24 10:48:06 +0530

    Create Solution.java
    WJP to read user name and display welcome message on the console.

---
+++ Solution.java
@@ -0,0 +1,17 @@
+// WJP to read user name and display welcome message on the console.
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) 
+{
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        System.out.println("welcome " +input);
+    
+}
+
+}
