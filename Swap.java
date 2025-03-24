1953b18bfb6179cef748d9a24b3bf3393350fca6
Sanjay Kumar Yadav <sanjay.script.uk@gmail.com>
2025-03-24 19:29:41 +0530

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,28 @@
+// Write a Java program to swap two variables.
+
+import java.util.*;
+
+class Swap 
+{
+
+	public static void main(String[] args)
+	{
+	int a = 20; 
+
+	int b = 50; 
+
+	int temp;
+
+	System.out.println("Printing values of A and B before swap: " + a + " " + b);
+
+	temp = a; 
+
+	a = b; 
+	
+	b = temp;
+	
+	System.out.println("Printing values of A and B after swap: " + a + " " + b);
+
+	}
+
+}
